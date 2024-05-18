abf5662d3206b49cb7d736888552c537000923d8
Ryan-shamu-YT <83490776+Ryan-shamu-YT@users.noreply.github.com>
2024-05-18 20:08:48 +0200

    Update servers.py

--- warp/servers.py
+++ warp/servers.py
@@ -5,5 +5,6 @@ All server addresses
 SERVERS = {
     "site": "tlds1.warp.thecommcraft.de",
     "tcc": "tlds1.warp.thecommcraft.de",
-    "net": "net-tld.warp.thecommcraft.de"
+    "net": "net-tld.warp.thecommcraft.de",
+    "com": "idk-com-i-guess.vercel.app"
 }
